977dafe739241d65793f220a744b459c2c508d7a
ganciaux <ghislain.anciaux@internet.lu>
2023-09-21 16:57:22 +0200

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,23 @@ import { Clients } from "./components/Clients";
 import { Articles } from "./components/Articles";
 import { Navigation } from "./components/Navigation";
 import { NoMatch } from "./components/NoMatch";
-
+import { Header } from "./components/Header";
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigation />}>
-          <Route path="home" element={<Home />} />
-          <Route path="clients" element={<Clients />} />
-          <Route path="articles" element={<Articles />} />
-          <Route path="*" element={<NoMatch />} />
-          {/* Ajoutez d'autres routes ici */}
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Navigation />}>
+            <Route path="home" element={<Home />} />
+            <Route path="clients" element={<Clients />} />
+            <Route path="articles" element={<Articles />} />
+            <Route path="*" element={<NoMatch />} />
+            {/* Ajoutez d'autres routes ici */}
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,13 +1,14 @@
+import { Container } from "@mui/material";
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
 
-export function Navigation({ Children }) {
+export function Navigation() {
   return (
-    <>
+    <Container maxWidth="lg">
       <Link to="/">Home</Link>
       <Link to="/clients">Clients</Link>
       <Link to="/articles">Article</Link>
       <Outlet />
-    </>
+    </Container>
   );
 }
